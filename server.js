e5189e84cf803b8a2d8a27846bbd4adda78eea40
pstewart <stewart.paul@gmail.com>
2021-02-24 09:29:59 -0500

    one version

--- server.js
+++ server.js
@@ -1,69 +1,16 @@
+const SPScraper = require('./sideprojectScraper')
 const telegram = require('./telegram')
 const axios = require('axios')
 const fs = require('fs')
-let oldValueSP = 1234567890
-let oldPayload = ''
-let msg = ''
-const getUrlSP = async () => {
-    try {
-        let payload = await axios.get('https://www.sideprojectbrewing.com/search.js?q=handle:*&view=bss.product.labels')
-        return payload.data
-    } catch (error) {
-        console.error(error)
-    }
-}
 
 
-const checkBeerSP = async () => {
+const runSP = async () => {
     try {
-        let payload = await getUrlSP()
-        let beerID = 6133564080281
-        let arrFound = payload.find (item => item.id == beerID)
-        let newValueSP = arrFound.inventory
-        console.dir(arrFound)
-        console.log('inventory: ' +typeof(arrFound.inventory))
-        console.log("Heartbeep. New value: " +newValueSP +". Old value: " +oldValueSP)
-        if ((oldValueSP != newValueSP) & (oldValueSP != 1234567890)) {
-            telegram.sendMsg("Ambiente sale went from " +oldValueSP +" to " +newValueSP)
-            console.log("Ambiente went from " +oldValueSP +" to " +newValueSP)
-        }
-        oldValueSP = newValueSP
+        let payload = await SPScraper.downloadURL()
+        SPScraper.runCompare(payload)
     } 
      catch (error) {
        console.error(error)
-    }
-}
-
-
-const checkUpdateSP = async () => {
-    try {
-        console.log("heartbeat..")
-        let payload = await getUrlSP()       
-        if (oldPayload != '') { // skip the init
-            payload.forEach(function(newkey) { 
-                let oldkey = oldPayload.find (item => item.id == newkey.id)
-                if (newkey.available != oldkey.available) {
-                    msg = 'Availability changed: ' +newkey.handle +' is now:' +newkey.available
-                    console.log(msg)
-                    telegram.sendMsg(msg)
-                }
-                if (newkey.inventory != oldkey.inventory) {
-                    msg = 'Inventory changed: ' +newkey.handle + '. from ' +oldkey.inventory + ' to ' +newkey.inventory
-                    console.log(msg)
-                    telegram.sendMsg(msg)                     
-                }
-                if (oldkey == null) {
-                    msg = 'NEW in store: ' +newkey.handle + '. Available set to:' +newkey.available
-                    console.log(msg)
-                    telegram.sendMsg(msg)     
-                }
-           })
-        }
-        oldPayload = payload
-    }
-    catch (error) {
-       console.error(error)
-       oldPayload = eval(payload)
     }
 }
 
@@ -71,10 +18,4 @@ const checkUpdateSP = async () => {
 /* MAIN 
 *********/
 console.log("start..")
-setInterval(checkUpdateSP, 5 * 60 * 1000)
-//checkUpdateSP()
-
-
-//telegram.sendMsg("Starting..")
-//checkBeerSP()
-//setInterval(checkBeerSP, 5 * 60 * 1000)
+setInterval(runSP, 1 * 5 * 1000)
--- sideprojectScraper.js
+++ sideprojectScraper.js
@@ -1,6 +1,11 @@
 const axios = require('axios')
+let oldInventory = ''
+let oldHandle = ''
+let oldLockStatus = ''
+let oldPayload = ''
+let msg = ''
 
-const checkChange = async (id) => {
+const downloadURL = async () => {
     try {
         let payload = await axios.get('https://www.sideprojectbrewing.com/search.js?q=handle:*&view=bss.product.labels')
         return payload.data
@@ -9,4 +14,43 @@ const checkChange = async (id) => {
     }
 }
 
-module.exports.checkChange = checkChange
+const runCompare = async (payload) => {
+    try {
+        console.log("heartbeat..")
+        //let payload = await downloadURL()       
+        if (oldPayload != '') { // skip the init
+            payload.forEach(function(newkey) { 
+                let oldkey = oldPayload.find (item => item.id == newkey.id)
+
+                if (newkey.available != oldkey.available) {
+                    msg = 'Availability changed: ' +newkey.handle +' is now:' +newkey.available
+                    console.log(msg)
+                    telegram.sendMsg(msg)
+                }
+                if (newkey.inventory != oldkey.inventory) {
+                    msg = 'Inventory changed: ' +newkey.handle + '. from ' +oldkey.inventory + ' to ' +newkey.inventory
+                    console.log(msg)
+                    telegram.sendMsg(msg)                     
+                }
+                if (oldkey == null) {
+                    msg = 'NEW in store: ' +newkey.handle + '. Available set to:' +newkey.available
+                    console.log(msg)
+                    telegram.sendMsg(msg)
+                }
+                if (JSON.stringify(newkey.tags) != (JSON.stringify(oldkey.tags))) {
+                    msg = 'TAGS Changed for ' +newkey.handle +": " + JSON.stringify(newkey.tags)
+                    console.log(msg)
+                    telegram.sendMsg(msg)
+                }
+           })
+        }
+        oldPayload = payload
+    }
+    catch (error) {
+       console.error(error)
+       oldPayload = eval(payload)
+    }
+}
+
+module.exports.downloadURL = downloadURL
+module.exports.runCompare = runCompare
